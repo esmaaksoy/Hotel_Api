5792eff82924311ee0b754e1f2f995ffe459b156
esma <esma.ea.aksoy@gmail.com>
2024-03-29 21:27:12 +0300

    feat: room model

--- src/models/room.js
+++ src/models/room.js
@@ -1,3 +1,29 @@
-"use strict"
+"use strict";
 
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
+const RoomSchema = new mongoose.Schema(
+  {
+    roomNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    images: [],
+    bedType: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    collection: "rooms",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Room", RoomSchema);
