6d9b4243cfedc9dabf749e1a0e962d095db49a7c
esma <esma.ea.aksoy@gmail.com>
2024-03-29 22:45:35 +0300

    feat: token

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,9 +1,19 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
 
-/* ------------------------------------------------------- */
+const router = require('express').Router()
+
+const token = require('../controllers/token')
+
+
+router.route('/')
+    .get(token.list)
+    .post(token.create)
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete)
+
+
 module.exports = router
